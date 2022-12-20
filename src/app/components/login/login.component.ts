fc11e0a7204566239b1005cdbb29245f760dc0ba
abnobrega <abonturi@gmail.com>
2022-12-20 16:20:22 -0300

    Validação de Formulário para LOGIN implementado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credencias } from 'src/app/models/credenciais';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  /*************/  
+  /* ATRIBUTOS */
+  /*************/ 
+  credencial: Credencias = {
+    email: '',
+    senha: ''
+  }
+
+  /* valida se o email é válido */
+  email = new FormControl(null, Validators.email);
+  /* valida se a senha tem um mínimo de 3 caracters */  
+  senha = new FormControl(null, Validators.minLength(3));  
+
+  /**************/  
+  /* CONSTRUTOR */
+  /**************/  
+  constructor() {
+
+  }
+
+  /***********/    
+  /* MÉTODOS */
+  /***********/   
+  ngOnInit(): void {
+
+  }
+
+  validarCampos(): boolean {
+    if (this.email.valid && this.senha.valid) {
+      return true;
+    } else {
+      return false;
+    }    
+  }
 
 }
